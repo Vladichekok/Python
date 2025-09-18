816f288f6051700b217ee1df8cb5fb6b37bf4e98
Vladichekok <90748765+Vladichekok@users.noreply.github.com>
2025-09-18 20:55:28 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from string_utils import print_string, check_case, smogtether_upper
+
+if __name__ == "__main__":
+    print_string("Hello world!")
+    check_case("HELLO")
+    check_case("hello")
+    check_case("HelloWorld")
+    print("Letter list:", smogtether_upper())
